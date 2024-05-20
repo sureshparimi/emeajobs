e511dec7e59a69ab564e88a8175947932d308497
Suresh Parimi <42301301+sureshparimi@users.noreply.github.com>
2024-05-20 22:26:23 +0200

    Update emeajobs.py

--- emeajobs.py
+++ emeajobs.py
@@ -30,10 +30,11 @@ def fetch_job_data(db):
         job_data.extend(doc_data.get("jobs", []))
     return job_data
 
+# Function to display rows as cards
 def display_cards(df):
     num_cols = 4  # Number of cards per row
     num_rows = len(df) // num_cols + (len(df) % num_cols > 0)
-    colors = ['#FAD02E', '#33A1DE', '#F05365', '#85BB65', '#AA5FB3', '#FF8C42', '#0F9D58', '#4FC3F7', '#F06292']
+    colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#f1c40f', '#e67e22', '#e74c3c', '#95a5a6']
 
     card_style = """
         <style>
@@ -46,27 +47,12 @@ def display_cards(df):
                 flex: 1;
                 display: flex;
                 flex-direction: column;
-                height: auto;
-                width: 300px; /* Increase the width as needed */
-                margin-bottom: 20px; /* Add margin to separate cards vertically */
-                margin-right: 20px; /* Add margin to separate cards horizontally */
-                padding-bottom: 10px; /* Add padding to separate cards horizontally */
             }
             .card h3 {
                 color: white;
-                padding: 10px;
-                border-top-left-radius: 10px;
-                border-top-right-radius: 10px;
-                margin-top: -20px;
             }
             .card p {
                 color: #7f8c8d;
-            }
-            .card .description {
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-                padding: 10px;
             }
             .card .header {
                 border-radius: 10px 10px 0 0;
@@ -103,11 +89,13 @@ def display_cards(df):
 
                     card_content = f"""
                         <div class="card">
-                            <h3 style="background-color: {background_color};">{df.loc[idx, 'job-title']}</h3>
+                            <div class="header" style="background-color: {background_color};">
+                                <h3>{df.loc[idx, 'job-title']}</h3>
+                            </div>
                             <p><b>Company:</b> {df.loc[idx, 'company']}</p>
                             <p><b>Location:</b> {df.loc[idx, 'location']}</p>
                             <p><b>Date Posted:</b> {df.loc[idx, 'posted-time-ago']}</p>
-                            <div class="description">{df.loc[idx, 'Job_txt'][:100]}...</div>
+                            <p><b>Job Description:</b> {df.loc[idx, 'Job_txt'][:50]}...</p>
                             <p><b>Contact:</b> {highlight_emails(contact_info)}</p>
                             <div style="{ticker_style}">{posted_time_ago}</div>
                             <a href="{job_link}" target="_blank"><div class="footer"><b>Click here to view the job</b></div></a>
@@ -133,7 +121,6 @@ def display_contact_card():
             <h3 style="color: white; background-color: #6A0DAD; padding: 5px; border-top-left-radius: 10px; border-top-right-radius: 10px; text-align: center;">Contact</h3>
             <div style="padding: 10px;">
                 <p style="color: black;"><b>Developer Name:</b> Suresh Parimi</p>
-                <p style="color: black;"><b>✉️ E-Mail:</b>sparimi@sparimi.com</p>
                 <div style="border: 2px solid #8E44AD; border-radius: 10px; padding: 10px; margin-top: 10px;">
                     <h4 style="color: #6A0DAD;">Connect with me:</h4>
                     <p><a href="https://linkedin.com/in/sparimi" target="_blank" style="color: #6A0DAD; text-decoration: none;">Linkedin</a></p>
@@ -149,8 +136,7 @@ def check_very_old(posted_time):
     if 'month' in posted_time:
         months_ago = int(posted_time.split()[0])
         if months_ago >= 1:
-            return
-            True
+            return True
     return False
 
 # Streamlit App main function
@@ -161,7 +147,6 @@ def main():
     st.sidebar.header("Filter Jobs")
     job_title = st.sidebar.text_input("Enter Job Title", "").strip()
     location = st.sidebar.text_input("Enter Location", "").strip()
-    show_recruiter_email = st.sidebar.checkbox("Show jobs with recruiter email address")
 
     job_data = fetch_job_data(db)
     df = pd.DataFrame(job_data)
@@ -172,10 +157,6 @@ def main():
 
     # Extract email addresses and create 'contact' column
     df['contact'] = df['Job_txt'].apply(lambda x: ', '.join(extract_emails(x)))
-
-    # Filter based on recruiter email address
-    if show_recruiter_email:
-        df = df[df['contact'].apply(lambda x: bool(re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', x)))]
 
     # Rearrange columns and reset index
     df = df[['job-title', 'company', 'contact', 'Job_Link', 'location', 'Job_txt', 'posted-time-ago']]
@@ -189,3 +170,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
